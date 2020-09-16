22157caeb1abe4dac30527d0005e8abc78a882b6
Zuza <37311848+misss13@users.noreply.github.com>
2020-09-16 21:27:27 +0200

    Update Servo_and_server.ino

--- Servo_and_server.ino
+++ Servo_and_server.ino
@@ -14,17 +14,12 @@ String statu = "";
 String output27State = "on";
 
 void setup() {
-  Serial.begin(115200);
   servo1.attach(servoPin);
 
-  Serial.print("Setting AP (Access Point)…");
   // Jak ma być AP to wystarczy wywalić haselo
   WiFi.softAP(ssid, password);
 
   IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
-  server.begin();
   
   //ustawia serwo na 
   pos=servo1.read();
@@ -48,27 +43,20 @@ void setup() {
     }
 }
 void loop() {
-  WiFiClient client = server.available();   // Listen for incoming clients
+  WiFiClient client = server.available();
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
+  if (client) {                             
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
         header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
+        if (c == '\n') {
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
-            //KRADZIONY KAWAŁEK KODU Z LOGAMI TEGO CO SIĘ DZIEJE
             
             // STRONKA
             client.println("<!DOCTYPE html><html>");
@@ -108,11 +96,9 @@ void loop() {
             }
             client.println("</body></html>");
 
-            // The HTTP response ends with another blank line
             client.println();
-            // Break out of the while loop
             break;
-          } else { // if you got a newline, then clear currentLine
+          } else {
             currentLine = "";
           }
         } else if (c != '\r') {
@@ -121,9 +107,7 @@ void loop() {
       }
 
     }
-    // Clear the header variable
     header = "";
-    // Close the connection
     client.stop();
     Serial.println("Client disconnected.");
     Serial.println("");
